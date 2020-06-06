deaf759f8bd05b916bb7741e7d135edd71602c2e
spraisewu <spraisewu@gmail.com>
2020-06-06 18:36:29 +0800

    updata

--- filelists/Aircraft/download_aircraft.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-wget http://www.robots.ox.ac.uk/~vgg/data/fgvc-aircraft/archives/fgvc-aircraft-2013b.tar.gz
-tar -zxvf fgvc-aircraft-2013b.tar.gz
-python classifier.py
-python write_aircraft_filelist.py
